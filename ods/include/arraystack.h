f7f768fe5bbfdb0625a65af79adb3508f6bbab45
Taito Ohsumi <ohsumi-taito035@g.ecc.u-tokyo.ac.jp>
2022-05-17 11:54:55 +0900

    define arraystack_t

--- ods/include/arraystack.h
+++ ods/include/arraystack.h
@@ -1,4 +1,21 @@
 #ifndef ARRAY_STACK_H
 # define ARRAY_STACK_H
 
+#include <stdlib.h>
+#include <assert.h>
+
+/*
+   PARAMETARS
+	elem_size    memory size of each element in array.
+	length       number of elements in array.
+	alloc_length current allocated memory size in array.
+	array        pointer to the first content of array.
+*/
+typedef struct {
+	size_t elem_size;
+	size_t length;
+	size_t alloc_length;
+	void *array;
+} arraystack_t;
+
 #endif
